a7b1d33269524fec807461e969fd7200e373d5f4
filippos dimitriadis <fildimitriadis1998@gmail.com>
2023-12-05 22:25:40 +0200

    minor 5 12 2023

--- dealership-client/src/app/interceptors/general.interceptor.ts
+++ dealership-client/src/app/interceptors/general.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { TokenService } from "../services/token.service";
-import { Observable, catchError, throwError } from "rxjs";
+import { Observable, catchError, tap, throwError } from "rxjs";
 import { Router } from "@angular/router";
 
 
@@ -16,13 +16,18 @@ export class GeneralInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         return next.handle(req).pipe(
-            catchError((error: any) => {
-                if (error instanceof HttpErrorResponse && error.status === 401) {
-                    this.tokenService.removeToken();
-                    this.router.navigateByUrl('');
-                }
-                return throwError(() => new Error(error));
-            })
+
+            tap({
+                error: x => {
+                    console.log(JSON.stringify(x));
+
+                    if (x instanceof HttpErrorResponse && x.status === 401) {
+                        this.tokenService.removeToken();
+                        this.router.navigateByUrl('');
+                    }
+                },
+                complete: () => console.log('completed'),
+            }),
         );
     }
 }
